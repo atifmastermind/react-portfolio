f27f6c994f405ab01dfec336101b2aabca6de6f2
Muhammad Atif <Atifmastermind12@gmail.com>
2024-10-01 15:51:30 +0500

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Products from "./components/Products";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <>
@@ -19,6 +20,7 @@ const App = () => {
           <Technologies />
           <Products />
           <Experience />
+          <Contact />
         </div>
       </div>
     </>
